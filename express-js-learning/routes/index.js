6349b4cbb17f4fc32659a115bd0655270c2d9ab8
bac-ta <bac93.it@gmail.com>
2020-04-06 13:12:59 +0700

    update source

--- express-js-learning/routes/index.js
+++ express-js-learning/routes/index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
+//Middle ware
+const log = (req, res, next) => {
+    console.log('Home logged');
+    next();
+};
+app.use(log);
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
--- express-js-learning/routes/users.js
+++ express-js-learning/routes/users.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
+
+//Middleware
+var requestTime = (req, res, next) => {
+    req.requestTime = Date.now();
+    next();
+};
+
+app.use(requestTime);
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
