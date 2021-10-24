354be4f5691751915c9fca8d3ba9cd1becf5e4e4
oTaDuyBac <taduy.bac@access-company.com>
2021-10-24 22:51:36 +0700

    update

---
+++ flutter_tutorial/lib/src/models/enums/login_user_type.dart
@@ -0,0 +1,1 @@
+enum LoginUserType { EMAIL, NAME, PHONE }
---
+++ flutter_tutorial/lib/src/screens/login_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _isLoading = false;
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+  signIn(String email, String password) async {
+
+  }
+
+}
