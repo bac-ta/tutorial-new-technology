dab46570ddfcf4dcecd1fdeffa3b4b89a586b6fd
bac-ta1 <bac-ta@dimage.co.jp>
2022-07-29 10:57:24 +0700

    create project kotlin

---
+++ MySdk/settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My_Sdk"
+include(":shared")
---
+++ MySdk/shared/src/commonMain/kotlin/com/example/mysdk/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.mysdk
+
+expect class Platform() {
+    val platform: String
+}
